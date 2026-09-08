8424f2a9ca76171c4b0daf2e2167e8589a71aa99
giuseppe becchi <giuseppebecchi@gmail.com>
2026-09-08 18:11:22 +0200

    attivati reminders

--- app/routes/console.php
+++ app/routes/console.php
@@ -4,10 +4,14 @@ use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
 use Illuminate\Support\Facades\Schedule;
 
-Artisan::command('inspire', function () {
+/*Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
-})->purpose('Display an inspiring quote')->hourly();
+})->purpose('Display an inspiring quote')->hourly();*/
 
-/*Schedule::command('payments:send-reminders')
+Schedule::command('payments:send-reminders')
     ->dailyAt('09:00')
-    ->withoutOverlapping();*/
+    ->withoutOverlapping();
+
+Schedule::command('leads:send-follow-ups')
+    ->dailyAt('09:15')
+    ->withoutOverlapping();
